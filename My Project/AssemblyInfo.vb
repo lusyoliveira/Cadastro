54539ebc4a805823cc542893eb9d0eff64b307d2
Luciene Oliveira <Luciene Oliveira@CASA2>
2024-06-22 17:13:59 -0300

    Adicionar arquivos de projeto.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("Cadastro")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Microsoft")> 
+<Assembly: AssemblyProduct("Cadastro")> 
+<Assembly: AssemblyCopyright("Copyright © Microsoft 2014")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("046491cf-2c5e-46e7-9ce6-82fd94382fc3")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
---
+++ Script.js
@@ -0,0 +1,37 @@
+﻿function ValidarFormulario() {
+    var nom = document.getElementById("nome"),
+        idade = document.getElementById("idade"),
+        email = document.getElementById("email");
+    if (nome.value == "" || idade.value == "" || email.value == "") {
+        alert("Preencher todos os campos");
+    }
+    else {
+        confirm("Deseja Prosseguir?");
+    }
+}
+
+function SelecionaTodos() {
+    var selecionaTodos = document.getElementById("TodasPessoas");
+    var pessoas = document.getElementsByName("Pessoa");
+    for (var i = 0; i < pessoas.length; i++) {
+        pessoas[i].checked = selecionaTodos.checked;
+    }
+}
+
+function Deletar() {
+    var pessoas = document.getElementsByName("Pessoa");
+    var algumselecionado = false;
+    for (var i = 0; i < pessoas.length; i++) {
+        if (pessoas[i].checked) {
+            algumselecionado = true;
+            break;
+        }
+    }
+        
+    if (!algumselecionado) {
+        alert("Selecione ao menos uma pessoa!");
+    }
+    else if(confirm("Deseja deletar a(s) pessoa(s) selecionada (s)?")) {
+        alert("Pessoas deletadas com sucesso!");
+    }
+}
